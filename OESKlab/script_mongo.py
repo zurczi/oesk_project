fa9547d3081b0f0d8ed6c99d6620562b2217ffc9
Anna Żurczak <anna.zurczak@student.put.poznan.pl>
2019-12-27 17:05:17 +0100

    Added script python + mongo

--- OESKlab/script_mongo.py
+++ OESKlab/script_mongo.py
@@ -2,11 +2,24 @@ from datetime import datetime
 import pymongo
 
 
+def create_json(file):
+    samples = []
+    dates = []
+    id = 0
+    with open(file, encoding="ISO-8859-1") as f:
+        for line in f:
+            data = line.split('<SEP>')
+            readable = datetime.fromtimestamp(int(data[2]))
+            dates.append({"day": readable.strftime("%d"),
+                      "month": readable.strftime("%m"),
+                      "year": readable.strftime("%y"),
+                      "_id": id
+                      })
 
-
-def read_into_database(samples, dates, file):
-
-
+            samples.append({"user_id": data[0], "track_id": data[1], "listen_date": id})
+            id += 1
+    print(dates)
+    return samples, dates
 
 def main():
     myclient = pymongo.MongoClient("mongodb://localhost:27017/")
@@ -18,7 +31,11 @@ def main():
     samples = mydb["samples"]
     dates = mydb["dates"]
 
-    read_into_database(samples, dates, "C:\\Users\\48783\\Desktop\\mgr1\\OESKlab\\files\\"+fileName+".txt")
+    samples_data, dates_data = create_json("C:\\Users\\48783\\Desktop\\mgr1\\OESKlab\\files\\" + fileName + ".txt")
+    samples.insert_many(samples_data)
+    dates.insert_many(dates_data)
+    myclient.close()
 
+    
 if __name__ == "__main__":
     main()
